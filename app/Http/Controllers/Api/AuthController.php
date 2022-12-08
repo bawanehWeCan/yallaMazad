cbec5cb97ce8f51b3b0c854e5e73a0870fa5dc7e
Alaa Bawaneh <a.bawaneh@wecan.jo>
2022-12-08 13:44:47 +0300

    otp

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -249,4 +249,44 @@ class AuthController extends Controller
 
         return $this->returnSuccessMessage('Logged out succesfully!');
     }
+
+    public function sendOTP($phone)
+    {
+        $otp = 5555;
+        // $otp = mt_rand(1000, 9999);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "http://82.212.81.40:8080/websmpp/websms",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "user=Wecan&pass=Suh12345&sid=WayToDoctor&mno=" . $phone . "&text=Your OTP is " . $otp . " for your account&type=1&respformat=json",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer 2c1d0706b21b715ff1e5a480b8360d90"
+            ),
+        ));
+
+        curl_exec($curl);
+
+        curl_close($curl);
+
+        return $otp;
+    }
+
+    public function checkOTP($phone, $otp)
+    {
+        $user = User::where('phone', $phone)->first();
+
+        if ((string)$user->otp == (string)$otp) {
+            return true;
+        }
+
+        return false;
+    }
 }
