d6120d4fe45840b543e5aab52cd8b936868a143a
aya-matroud <ayamatroud8@gmail.com>
2022-12-21 06:16:20 -0800

    fix

--- app/Models/Advertisement.php
+++ app/Models/Advertisement.php
@@ -10,6 +10,15 @@ class Advertisement extends Model
     use HasFactory;
     protected $guarded=[];
 
+    public function setImageAttribute($value){
+        if ($value){
+            $file = $value;
+            $extension = $file->getClientOriginalExtension(); // getting image extension
+            $filename =time().mt_rand(1000,9999).'.'.$extension;
+            $file->move(public_path('img/'), $filename);
+            $this->attributes['image'] =  'img/'.$filename;
+        }
+    }
 
     public function user(){
         return $this->belongsTo(User::class);
