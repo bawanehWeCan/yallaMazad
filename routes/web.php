e5f5798ceed3ad17070f7a533e256644d8a69b0b
Alaa Bawaneh <a.bawaneh@wecan.jo>
2023-02-01 14:40:41 +0300

    firebase

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( 'test', [ AddressController::class, 'save' ] );
+Route::get('test', [AddressController::class, 'save']);
+
+
+// test for insert in firebase
+Route::get('firebase', function () {
+    $bid = app('firebase.firestore')->database()->collection('auctions')->document('some_id');// we will replace this value with auction id
+    $bid->set([
+        'created_at' => 'aaa'
+
+    ]);
+});
