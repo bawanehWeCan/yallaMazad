273df384b0b258985316adba6902ebb248bd9989
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-02-04 11:35:59 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get( 'test', [ AddressController::class, 'save' ] );
+Route::get('test', [AddressController::class, 'save']);
+
+//http://admin.yallamzad.com/firebase
+// test for insert in firebase
+Route::get('firebase', function () {
+
+    // we try to insert collection( table ) into record
+    //now we need to make this code for bid create
+    $bid = app('firebase.firestore')->database()->collection('auctions')->document('some_id')->collection('biddings')->document('some_child_id');// we will replace this value with auction id
+    $bid->set([
+        'created_at' => 'aaa'
+
+    ]);
+});
+
