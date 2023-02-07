299d56af95cd38df5be0fe5d8a0ee832cd02e46f
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-02-07 15:34:43 +0200

    Add files via upload

--- app/Http/Resources/AdvertisementResource.php
+++ app/Http/Resources/AdvertisementResource.php
@@ -18,17 +18,18 @@ class AdvertisementResource extends JsonResource
     public function toArray($request)
     {
 
-        $date = Carbon::now();
-$daysToAdd = 10;
-$date = $date->addDays($daysToAdd);
+//         $date = Carbon::now();
+// $daysToAdd = 10;
+// $date = $date->addDays($daysToAdd);
 
-        $fav = false;
-        if(Auth::user()){
-        $favorite = Favorite::where('user_id',Auth::user()->id)->where('advertisement_id',$this->id)->first();
-            if($favorite){
-                $fav = true ;
-            }
-    }
+//         $fav = false;
+//         if(Auth::user()){
+//         $favorite = Favorite::where('user_id',Auth::user()->id)->where('advertisement_id',$this->id)->first();
+//             if($favorite){
+//                 $fav = true ;
+//             }
+//     }
+
         return [
 
             'id'=>$this->id,
@@ -36,12 +37,15 @@ $date = $date->addDays($daysToAdd);
             'name'=>$this->name,
             'content'=>$this->content,
             'start_price'=>$this->start_price,
-            'start_date'=>(string)$this->created_at,
-            'end_date'=>$date ,
+            'start_date'=>(string)$this->start_date,
+            'end_date'=>(string)$this->end_date,
             'status'=>$this->status,
             'buy_now_price'=>$this->buy_now_price,
             'views'=>$this->views,
             'number_of_bids'=>$this->number_of_bids,
+            'price_one'=>$this?->price_one??0,
+            'price_two'=>$this?->price_two??0,
+            'price_three'=>$this?->price_three??0,
             'price_one'=>50,
             'price_two'=>100,
             'price_three'=>150,
