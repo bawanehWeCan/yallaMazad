3adbd1bbe03aee9cb81e086ece9642f3f9cd6ef9
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-02-20 15:51:38 +0200

    Add files via upload

--- app/Http/Controllers/Admin/AdvertisementCrudController.php
+++ app/Http/Controllers/Admin/AdvertisementCrudController.php
@@ -74,7 +74,7 @@ class AdvertisementCrudController extends CrudController
         }]);
         CRUD::addColumn(['name' => 'winner', 'label'=>'Advertisement Winner','type'     => 'closure',
         'function' => function(Advertisement $entry) {
-            return $entry?->bids()?->max('price')->user;
+            return $entry?->bids()?->max('price');
         }]);
        CRUD::column('image')->type('image');
        CRUD::addColumn(['name' => 'category', 'label'=>'Category','type'     => 'closure',
