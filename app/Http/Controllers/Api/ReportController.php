28c9da89141ff65e29088f58287b5e14a2f15332
aya-matroud <ayamatroud8@gmail.com>
2023-02-20 04:22:52 -0800

    addReport

---
+++ app/Http/Controllers/Api/ReportController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Report;
+use Illuminate\Http\Request;
+use App\Repositories\Repository;
+use App\Http\Requests\ReportRequest;
+use App\Http\Resources\ReportResource;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
+
+class ReportController extends ApiController
+{
+
+    public function __construct()
+    {
+        $this->resource = ReportResource::class;
+        $this->model = app(Report::class);
+        $this->repositry =  new Repository($this->model);
+    }
+
+    public function save( Request $request ){
+
+        $adv = Report::where('advertisement_id',$request->advertisement_id)->where('sender_id',$request->sender_id)->first();
+        $user = Report::where('user_id',$request->user_id)->where('sender_id',$request->sender_id)->first();
+        if($adv || $user){
+            return $this->returnError(__('Sorry! You cannot report again !'));
+        }
+        return $this->store( $request->all() );
+    }
+
+
+    public function edit($id,Request $request){
+
+
+        return $this->update($id,$request->all());
+
+    }
+
+}
---
+++ app/Models/Report.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+    use HasFactory;
+    protected $guarded=[];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function sender(){
+        return $this->belongsTo(User::class,'sender_id');
+    }
+
+    public function advertisement(){
+        return $this->belongsTo(Advertisement::class);
+    }
+}
