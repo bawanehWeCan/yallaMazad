21d23e5f442aa60ce9c91ddb9a579a24696df387
aya-matroud <ayamatroud8@gmail.com>
2023-03-08 01:24:05 -0800

    fixed

--- app/Http/Controllers/Api/BidController.php
+++ app/Http/Controllers/Api/BidController.php
@@ -61,7 +61,7 @@ class BidController extends ApiController
 
                     $model = $this->repositry->save($request->all());
 
-                          $user_ids = Bid::where('advertisement_id',$request->advertisement_id)->where('user_id',"!=",$request->user_id)->pluck('user_id')->all();
+                          $user_ids = Bid::where('advertisement_id',$request->advertisement_id)->where('user_id',"!=",$request->user_id)->pluck('user_id');
                             $tokens = User::whereIn('id',$user_ids)->whereNotNull('device_token')
                             ->pluck('device_token');
                             return $tokens;
