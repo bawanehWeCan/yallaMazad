b96401f72a0354d298a505c8677c2bfacb655f33
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-03-12 17:39:58 +0200

    Update NotificationTrait.php

--- app/Traits/NotificationTrait.php
+++ app/Traits/NotificationTrait.php
@@ -102,8 +102,6 @@ trait NotificationTrait
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
     $result = curl_exec($ch);
 //         if ($result === FALSE) {
