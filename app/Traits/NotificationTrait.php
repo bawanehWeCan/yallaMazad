122a5bf1bdecc68b6d4de53b253cc70c81742f54
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-03-13 16:11:43 +0200

    Update NotificationTrait.php

--- app/Traits/NotificationTrait.php
+++ app/Traits/NotificationTrait.php
@@ -95,7 +95,6 @@ trait NotificationTrait
         'Authorization: key=' . env('FIREBASE_API_KEY'),
         'Content-Type: application/json'
     );
-        dd($fields);
     //#Send Reponse To FireBase Server
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
