e775f4b5a839153732ae75b8b7b0a99113d2d26c
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-05-17 19:50:11 +0300

    Add files via upload

--- app/Traits/NotificationTrait.php
+++ app/Traits/NotificationTrait.php
@@ -52,77 +52,75 @@ trait NotificationTrait
         return true;
     }
 
-    public function adNotificationSend($id, $status,$title, $content,$token)
-{
-    $msg['Title']=$title;
-    $msg['Body']=$content;
-    $data = [
-        'id' => $id,
-        'advertisement' => $status,
-        'click_action'=> 'FLUTTER_NOTIFICATION_CLICK',
-    ];
+    public function adNotificationSend($id, $status, $title, $content, $token)
+    {
+        $msg['Title'] = $title;
+        $msg['Body'] = $content;
+        $data = [
+            'id' => $id,
+            'advertisement' => $status,
+            "click_action" => "FLUTTER_NOTIFICATION_CLICK",
+        ];
 
         $fields = array
             (
             'to' => $token,
             'notification' => $msg,
-            'data'=> $data,
-            );
+            'data' => $data,
+        );
 
+        $headers = array
+            (
+            'Authorization: key=' . env('FIREBASE_API_KEY'),
+            'Content-Type: application/json',
+        );
+        //#Send Reponse To FireBase Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        // dd(env('FIREBASE_API_KEY'));
+        curl_close($ch);
 
-    $headers = array
-        (
-        'Authorization: key=' . env('FIREBASE_API_KEY'),
-        'Content-Type: application/json'
-    );
-    //#Send Reponse To FireBase Server
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-    $result = curl_exec($ch);
-    // dd(env('FIREBASE_API_KEY'));
-    curl_close($ch);
+        return true;
+    }
 
-    return true;
-}
-
-public function addNewNotificationSend($content,$token)
-{
-    $msg['Title']='User Notification';
-    $msg['Body']=$content;
-    $data = [
-        'click_action'=> 'FLUTTER_NOTIFICATION_CLICK',
-    ];
+    public function addNewNotificationSend($content, $token)
+    {
+        $msg['Title'] = 'User Notification';
+        $msg['Body'] = $content;
+        $data = [
+            "click_action" => "FLUTTER_NOTIFICATION_CLICK",
+        ];
 
         $fields = array
             (
             'to' => $token,
             'notification' => $msg,
-            'data'=> $data,
-            );
+            'data' => $data,
+        );
 
+        $headers = array
+            (
+            'Authorization: key=' . env('FIREBASE_API_KEY'),
+            'Content-Type: application/json',
+        );
+        //#Send Reponse To FireBase Server
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+        $result = curl_exec($ch);
+        //dd($result);
+        curl_close($ch);
 
-    $headers = array
-        (
-        'Authorization: key=' . env('FIREBASE_API_KEY'),
-        'Content-Type: application/json'
-    );
-    //#Send Reponse To FireBase Server
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
-    $result = curl_exec($ch);
-    //dd($result);
-    curl_close($ch);
-
-    return true;
-}
+        return true;
+    }
 }
