9e22312c117519362814737423b9afeb3f7f2fcc
Mina-Emad234 <83023543+Mina-Emad234@users.noreply.github.com>
2023-05-18 20:41:00 +0300

    Add files via upload

--- app/Http/Controllers/Admin/SubscriptionCrudController.php
+++ app/Http/Controllers/Admin/SubscriptionCrudController.php
@@ -126,10 +126,11 @@ class SubscriptionCrudController extends CrudController
             $this->crud->getStrippedSaveRequest($request)
         );
         $this->data['entry'] = $this->crud->entry = $item;
-        dd((int)$this->data['entry']->plan->number_of_auction);
         $this->data['entry']->user()->update([
             'number_of_advs'=>(int)$this->data['entry']->plan->number_of_auction
         ]);
+           dd($this->data['entry']->user);
+
         // show a success message
         \Alert::success(trans('backpack::crud.update_success'))->flash();
 
