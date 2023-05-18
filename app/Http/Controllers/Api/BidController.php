f27e533d0b651a9f58e67f8ce62d5b7e190c32d8
aya-matroud <ayamatroud8@gmail.com>
2023-05-18 05:34:26 -0700

    fixed

--- app/Http/Controllers/Api/BidController.php
+++ app/Http/Controllers/Api/BidController.php
@@ -55,8 +55,6 @@ class BidController extends ApiController
                 if($counts==0 || $counts < $all || $user->number_of_advs > 0){
 
 
-                    // $delay = random_int(500, 5000); // Generate a random number between 0 and 2000 (milliseconds)
-                    usleep($user->id *10000); // Pause the script execution for the randomly generated number of milliseconds
 
 
                     $user = User::find( $request->user_id );
